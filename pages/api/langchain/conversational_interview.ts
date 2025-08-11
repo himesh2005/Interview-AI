2128a776a0797ab7cbe2c6d8ea01e16c96647309
himesh2005 <himeshchakrapani6@gmail.com>
2025-08-12 01:38:31 +0530

    conversational interview api complete

--- pages/api/langchain/conversational_interview.ts
+++ pages/api/langchain/conversational_interview.ts
@@ -6,7 +6,7 @@ import { ConversationChain } from "langchain/chains";
 import { MongoDBChatMessageHistory } from "@langchain/mongodb";
 import { PromptTemplate } from "@langchain/core/prompts";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
-//conversational interview API complete
+//conversational interview API completee
 const handler = async function (
   req: NextApiRequest,
   res: NextApiResponse
